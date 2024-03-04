0e15d9c0dd05360817859b11bd39aca19a6f1f39
Saurabh Saxena <saurabh.jan03@gmail.com>
2024-03-04 01:03:04 -0500

    adding the body parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
+const bodyParse = require("body-parser");
+const bodyParser = require('body-parser');
 const port = 3000
 
-app.get('/', function(req, res)  {
+app.use(bodyParser.json());
+app.post('/', function(req, res)  {
+  console.log(req.body);
   res.send('Hello World!')
 })
 
